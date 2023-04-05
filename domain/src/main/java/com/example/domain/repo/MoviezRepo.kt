f140422ebb94b5915f483f6b7ffb6372149dfc9b
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-04-06 01:16:59 +0200

    Created Moviez repo

---
+++ domain/src/main/java/com/example/domain/repo/MoviezRepo.kt
@@ -0,0 +1,15 @@
+package com.example.domain.repo
+
+import com.example.domain.entity.movieDetails.MovieDetailsResponse
+import com.example.domain.entity.movieList.MovieListResponse
+import com.example.domain.entity.nowPlaying.NowPlayingResponse
+
+interface MoviezRepo {
+    suspend fun getTopRated(): MovieListResponse
+
+    suspend fun getNowPlaying(): NowPlayingResponse
+
+    suspend fun getMovieDetails(): MovieDetailsResponse
+
+    suspend fun searchMoviesByName(): MovieListResponse
+}
