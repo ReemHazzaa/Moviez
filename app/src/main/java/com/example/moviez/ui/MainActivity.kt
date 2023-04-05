dc2c2d8cddf94e4ed6714bd32025db8bd837ab3f
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-04-05 23:23:42 +0200

    Create data & domain modules

--- app/src/main/java/com/example/moviez/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviez/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.moviez
+package com.example.moviez.ui
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.moviez.R
 import com.example.moviez.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
