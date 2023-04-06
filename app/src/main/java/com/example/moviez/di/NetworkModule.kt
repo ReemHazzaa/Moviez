ea97199961efa4b8f1cf28a766a254e86c027226
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-04-06 21:02:18 +0200

    Done some refactoring

--- app/src/main/java/com/example/moviez/di/NetworkModule.kt
+++ app/src/main/java/com/example/moviez/di/NetworkModule.kt
@@ -18,7 +18,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
-
     @Singleton
     @Provides
     fun provideOkHttpLoggingInterceptor(): HttpLoggingInterceptor {
--- app/src/main/java/com/example/moviez/utils/genericadapter/listener/OnItemClickCallback.kt
+++ app/src/main/java/com/example/moviez/utils/genericadapter/listener/OnItemClickCallback.kt
@@ -1,7 +1,7 @@
 package com.example.moviez.utils.genericadapter.listener
 
 import android.view.View
-import com.nwc.employeecare.presentation.utils.genericadapter.Listable
+import com.example.moviez.utils.genericadapter.Listable
 
 interface OnItemClickCallback {
     fun onItemClicked(view: View, listableItem: Listable, position: Int)
