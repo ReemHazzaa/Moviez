f38640d2bac8e2728a5d9d8d4d54ca3c96f572d2
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-04-07 17:15:36 +0200

    Added Fav room entity class

--- app/src/main/java/com/example/moviez/app/entity/FavMovieItem.kt
+++ app/src/main/java/com/example/moviez/app/entity/FavMovieItem.kt
@@ -1,18 +1,22 @@
 package com.example.moviez.app.entity
 
 import androidx.databinding.library.baseAdapters.BR
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.moviez.R
 import com.example.moviez.app.utils.genericadapter.HolderClass
 import com.example.moviez.app.utils.genericadapter.Listable
 import com.example.moviez.app.viewHolder.TopRatedViewHolder
 
-data class MovieItem(
+@Entity(tableName = "favorites_table")
+data class FavMovieItem(
+    @PrimaryKey(autoGenerate = false)
     val id: Int,
     val title: String,
     val imageUrl: String,
     val voteAvg: String,
 ) : Listable {
     override val listItemType: HolderClass =
-        HolderClass(TopRatedViewHolder::class.java, R.layout.item_top_rated)
-    override val variableId: Int = BR.movie
+        HolderClass(TopRatedViewHolder::class.java, R.layout.item_fav)
+    override val variableId: Int = BR.favMovie
 }
