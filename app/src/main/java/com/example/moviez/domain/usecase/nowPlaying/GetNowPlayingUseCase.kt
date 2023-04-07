ef212368ec62c8397a23f5c7fcbcf93e97b17928
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-04-07 04:49:13 +0200

    Update GetNowPlayingUseCase.kt

--- app/src/main/java/com/example/moviez/domain/usecase/nowPlaying/GetNowPlayingUseCase.kt
+++ app/src/main/java/com/example/moviez/domain/usecase/nowPlaying/GetNowPlayingUseCase.kt
@@ -1,18 +1,22 @@
 package com.example.moviez.domain.usecase.nowPlaying
 
+import com.example.moviez.app.entity.MovieItem
 import com.example.moviez.domain.entity.nowPlaying.NowPlayingResponse
 import com.example.moviez.domain.repo.MoviezRepo
 import com.example.moviez.domain.usecase.BaseUseCase
 import javax.inject.Inject
 
 class GetNowPlayingUseCase @Inject constructor(private val moviezRepo: MoviezRepo) :
-    BaseUseCase<GetNowPlayingUseCase.Params, NowPlayingResponse>() {
+    BaseUseCase<GetNowPlayingUseCase.Params, List<MovieItem>>() {
 
     data class Params(
         val page: Int = 1
     )
 
-    override suspend fun execute(params: Params): NowPlayingResponse {
-        return moviezRepo.getNowPlaying(params.page)
+    override suspend fun execute(params: Params): List<MovieItem> {
+        val result = moviezRepo.getNowPlaying(params.page)
+        return result.results.mapIndexed { index, movie ->
+            MovieItem(movie.id, movie.title, movie.poster_path, movie.vote_average.toString())
+        }
     }
 }
